a7055366d52084016a210de41326335b51ece403
Ivan Limarev <110815117+Benvire22@users.noreply.github.com>
2024-05-10 18:52:21 +0600

    Update task-01-init-projects.sh
    Edti task-01-file

--- task-01-init-projects.sh
+++ task-01-init-projects.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ./projects/my-project/
-chmod -R 755 ./projects  
+chmod -R 764 ./projects  
 cd ./projects/my-project/
 mkdir -p ./directory1/subdirectory1 ./directory2 ./directory3/subdirectory2
 touch ./directory2/hello.txt && echo "Hello from JS" > ./directory2/hello.txt
